47f1e41f9d7c1f079ee264b3b637fad04ebc2852
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-11-10 19:48:41 -0300

    deploiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Body, Container, Header, HeaderTitle } from './App.styles';
 import { Item } from './@types/Item';
-import { Category } from './@types/Category';
 import { itens } from './data/itens';
 import { categories } from './data/categories';
 import { filteredListByMonth, getCurrentMonth } from './helpers/dateFilters'
 import TableArea from './components/TableArea';
 import { InfoArea } from './components/InfoArea';
 const App = () => {
-  const [list, setList] = useState(itens);
+  const [list, setList] = useState<Item[]>([]);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrencyMonth] = useState(getCurrentMonth());
   const [income, setIncome] = useState(0);
@@ -19,10 +18,12 @@ const App = () => {
   }
 
   useEffect(() => {
+    setList(itens)
     setFilteredList(filteredListByMonth(list, currentMonth))
   }, [list, currentMonth]);
 
   useEffect(() => {
+
     let TotalIncome = 0;
     let TotalExpense = 0;
     for (let i in filteredList) {
