fee9180a7a7114d8212b7f8901110d5eb2eb28e3
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-11-10 21:01:04 -0300

    enviando novos dados

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,5 +3,10 @@ import { Category } from '../@types/Category';
 export const categories: Category = {
     food: { title: 'Alimentação', color: 'blue', expense: true },
     rent: { title: 'Aluguel', color: 'brown', expense: true },
-    salary: { title: 'Salário', color: 'green', expense: false }
+    salary: { title: 'Salário', color: 'green', expense: false },
+    car: { title: 'Combustivel', color: 'red', expense: true },
+    house: { title: 'Energia', color: 'red', expense: true },
+    internet: { title: 'Freelancer', color: 'green', expense: false },
+    phone: { title: 'Instagram', color: 'purple', expense: false },
+    celphone: { title: 'Conta', color: 'blue', expense: true },
 }
--- src/data/itens.ts
+++ src/data/itens.ts
@@ -1,8 +1,17 @@
 import { Item } from '../@types/Item';
 
 export const itens: Item[] = [
+    /*
+        formato da data é de acordo com a posição do array de meses
+        ex: 11 = dezembro
+    */
+
     { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12 },
     { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
     { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
     { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { date: new Date(2021, 10, 18), category: 'car', title: 'Combustivel', value: 2000 },
+    { date: new Date(2021, 11, 18), category: 'internet', title: 'Freela', value: 6000 },
+    { date: new Date(2021, 11, 18), category: 'phone', title: 'Propagandas', value: 900 },
+    { date: new Date(2021, 11, 18), category: 'house', title: 'Contas da casa', value: 500 },
 ];
